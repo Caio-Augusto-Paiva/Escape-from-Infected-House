e20b430b439495e01725991f2fc010d3b5b93d3f
Caio-Augusto-Paiva <caioaugusto.paiva@estudante.ufjf.br>
2026-01-28 12:56:11 -0300

    Resolvendo bugs

--- escape-from-infected-house/scripts/item_arma.gd
+++ escape-from-infected-house/scripts/item_arma.gd
@@ -3,8 +3,28 @@ extends Area3D
 # Qual arma esse item entrega?
 @export_enum("Pistola", "SMG", "Shotgun") var nome_arma = "Shotgun"
 
+@onready var vis_pistola = $Visual_Pistola
+@onready var vis_smg = $Visual_SMG
+@onready var vis_shotgun = $Visual_Shotgun
+
 func _ready():
 	body_entered.connect(_on_body_entered)
+	atualizar_visual()
+
+func atualizar_visual():
+	# Esconde todas as armas
+	if vis_pistola: vis_pistola.visible = false
+	if vis_smg: vis_smg.visible = false
+	if vis_shotgun: vis_shotgun.visible = false
+	
+	# Mostra a arma correta
+	match nome_arma:
+		"Pistola":
+			if vis_pistola: vis_pistola.visible = true
+		"SMG":
+			if vis_smg: vis_smg.visible = true
+		"Shotgun":
+			if vis_shotgun: vis_shotgun.visible = true
 
 func _on_body_entered(body):
 	if body.name == "Player":
